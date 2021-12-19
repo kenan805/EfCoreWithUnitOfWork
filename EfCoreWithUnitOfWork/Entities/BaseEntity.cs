95ec3d0f45ebb74834df41f4248c87e4313f1e75
Kənan İdayatov <63493758+Kenan805@users.noreply.github.com>
2021-12-19 14:39:54 +0400

    Update BaseEntity.cs

--- EfCoreWithUnitOfWork/Entities/BaseEntity.cs
+++ EfCoreWithUnitOfWork/Entities/BaseEntity.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace EfCoreWithUnitOfWork.Entities
 {
-    public class BaseEntity
+    public abstract class BaseEntity
     {
         public int Id { get; set; }
     }
